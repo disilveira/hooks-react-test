3b25c16f7530cd13dca844ddea24f832b73996a9
Diego Almeida <diego.almeida@outlook.com>
2020-11-04 15:44:00 -0300

    Added a API resource

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://apicarros.com/v1/consulta/qqt5645/json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems([result]);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error){
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="App">
+        <h1>React Hook Test</h1>
+        <h3>Car API</h3>
+        <ul>
+            {items.map(item => (
+              <li key={item.placa}>
+                {item.placa} {item.modelo}
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  
 }
 
 export default App;
